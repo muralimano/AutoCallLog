c162a8899e05787f1a54ed31c7cb066cbc36ab57
muralimano <muralimanoharsoft@gmail.com>
2018-12-28 12:55:39 +0530

    main activity

--- app/src/main/java/com/muralimanohar/autocalllog/MainActivity.java
+++ app/src/main/java/com/muralimanohar/autocalllog/MainActivity.java
@@ -11,89 +11,127 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = null;
     TextView call;
+    private static final int MY_PERMISSIONS_REQUEST_ACCOUNTS = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         call = findViewById(R.id.call);
+        checkAndRequestPermissions();
         getCallDetails();
     }
+
+    // Check Necessary Permission
+    private boolean checkAndRequestPermissions() {
+        int readcontactPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS);
+
+        int readphonestatePermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE);
+
+        int readphonelogPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CALL_LOG);
+
+        int writecalllogPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_CALL_LOG);
+
+        int readexternalstoragePermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (readcontactPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_CONTACTS);
+        }
+        if (readphonestatePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (readphonelogPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_CALL_LOG);
+        }
+        if (writecalllogPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_CALL_LOG);
+        }
+        if (readexternalstoragePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MY_PERMISSIONS_REQUEST_ACCOUNTS);
+            return false;
+        }
+
+        return true;
+    }
+
+
+    //    ArrayList<String> contactmiss = new ArrayList<String>();
+    ArrayList<contactdetails> contactmiss = new ArrayList<contactdetails>();
+
+
     private void getCallDetails() {
 
-        final String[] NECESSARY_PERMISSIONS = new String[]{Manifest.permission.GET_ACCOUNTS};
+//        StringBuffer sb = new StringBuffer();
+        Cursor managedCursor = managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null);
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
+//        sb.append( "Call Details :");
+        while (managedCursor.moveToNext()) {
+            String callType = managedCursor.getString(type);
+            contactdetails contactdd = new contactdetails();
+            contactmiss.add(managedCursor.getString(number));
+            contactmiss.add(managedCursor.getString(duration));
+            contactmiss.add(managedCursor.getString(date));
+//            String phNumber = managedCursor.getString(number);
+//            String callType = managedCursor.getString(type);
+//            String callDate = managedCursor.getString(date);
+//            Date callDayTime = new Date(Long.valueOf(callDate));
+//            String callDuration = managedCursor.getString(duration);
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED) {
-            Log.i("message", "permision granted");
-            //Permission is granted
+            Log.i(TAG, "getCallDetails: "+contactmiss);
 
-        } else {
-
-            //ask for permission
-
-            ActivityCompat.requestPermissions(
-                    MainActivity.this,
-                    NECESSARY_PERMISSIONS, 123);
-        }
-        StringBuffer sb = new StringBuffer();
-        Cursor managedCursor = managedQuery( CallLog.Calls.CONTENT_URI,null, null,null, null);
-        int number = managedCursor.getColumnIndex( CallLog.Calls.NUMBER );
-        int type = managedCursor.getColumnIndex( CallLog.Calls.TYPE );
-        int date = managedCursor.getColumnIndex( CallLog.Calls.DATE);
-        int duration = managedCursor.getColumnIndex( CallLog.Calls.DURATION);
-        sb.append( "Call Details :");
-        while ( managedCursor.moveToNext() ) {
-            String phNumber = managedCursor.getString( number );
-            String callType = managedCursor.getString( type );
-            String callDate = managedCursor.getString( date );
-            Date callDayTime = new Date(Long.valueOf(callDate));
-            String callDuration = managedCursor.getString( duration );
-            String dir = null;
-            int dircode = Integer.parseInt( callType );
-            switch( dircode ) {
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
                 case CallLog.Calls.OUTGOING_TYPE:
-                    dir = "OUTGOING";
+                    contactmiss.add("OUTGOING");
                     break;
 
                 case CallLog.Calls.INCOMING_TYPE:
-                    dir = "INCOMING";
+                    contactmiss.add("INCOMING");
                     break;
 
                 case CallLog.Calls.MISSED_TYPE:
-                    dir = "MISSED";
+                    contactmiss.add("MISSED");
                     break;
             }
 
+
             if (dircode == 3) {
-                sb.append( "\nPhone Number:--- "+phNumber +" \nCall Type:--- "+dir+" \nCall Date:--- "+callDayTime+" \nCall duration in sec :--- "+callDuration );
-                sb.append("\n----------------------------------");
+
+
             }
-//            else if (dircode == 2){
-//                sb.append( "\nPhone Number:--- "+phNumber +" \nCall Type:--- "+dir+" \nCall Date:--- "+callDayTime+" \nCall duration in sec :--- "+callDuration );
-//                sb.append("\n----------------------------------");
-//            }
-//            else if (dircode == 3){
-//                sb.append( "\nPhone Number:--- "+phNumber +" \nCall Type:--- "+dir+" \nCall Date:--- "+callDayTime+" \nCall duration in sec :--- "+callDuration );
-//                sb.append("\n----------------------------------");
-//            }
 
-
-//            sb.append( "\nPhone Number:--- "+phNumber +" \nCall Type:--- "+dir+" \nCall Date:--- "+callDayTime+" \nCall duration in sec :--- "+callDuration );
-//            sb.append("\n----------------------------------");
         }
         managedCursor.close();
 
-        call.setText(sb);
+            call.setText(itr.next());
+
+//      call.setText(contactmiss);
 
 
     }
 
 
 }
+
 
 
